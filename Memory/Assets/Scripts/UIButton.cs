e3af60ccf8bc29b5bc4d113e830323da2c5a9455
VladIsLove16 <zalezshi@gmail.com>
2022-03-29 18:59:06 +0500

    r

---
+++ Memory/Assets/Scripts/UIButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    [SerializeField]private GameObject targetObject;
+    [SerializeField]private string targetMessage;
+    public Color highlightColor =Color.cyan;
+    // Start is called before the first frame update
+   public void OnMouseOver() {
+       SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+       if( sprite!=null){
+           sprite.color=highlightColor;
+
+       }
+   }
+public void OnMouseExit() {
+    SpriteRenderer sprite=GetComponent<SpriteRenderer>();
+    if(sprite!=null){
+        sprite.color=Color.white;
+    }
+}
+public void OnMouseDown() {   
+        transform.localScale=new Vector3(1.1f,1.1f,1.1f);
+}
+public void OnMouseUp() {
+    transform.localScale=Vector3.one;
+    if (targetObject!=null)
+    targetObject.SendMessage(targetMessage);
+}
+}
+   
+
